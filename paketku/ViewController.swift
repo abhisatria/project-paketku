ddf00bb64f7beec23d1729733c44b3fc73f65786
abhisatria <abhiwisesa@gmail.com>
2020-12-15 05:43:25 -0800

    Initial Commit

---
+++ paketku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  paketku
+//
+//  Created by Abhi Wisesa on 15/12/20.
+//  Copyright © 2020 Abhi Wisesa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
