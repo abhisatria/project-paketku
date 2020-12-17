412ae834ab87a32f9e35d9c9fb87173052184c89
garrystevens007 <49149545+garrystevens007@users.noreply.github.com>
2020-12-17 17:37:09 +0700

    Paketku Update UI

--- paketku/PaketTableViewCell.swift
+++ paketku/PaketTableViewCell.swift
@@ -11,14 +11,21 @@ import UIKit
 class PaketTableViewCell: UITableViewCell {
 
     @IBOutlet weak var imgLogo: UIImageView!
-    @IBOutlet weak var txtProduct: UILabel!
-    
+    @IBOutlet weak var viewCard: UIView!
     @IBOutlet weak var txtResi: UILabel!
-    
-    @IBOutlet weak var txtStatus: UILabel!
+    @IBOutlet weak var txtPengirim: UILabel!
+    @IBOutlet weak var txtPenerima: UILabel!
+    @IBOutlet weak var viewInfo: UIView!
+    @IBOutlet weak var lblInfo: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        viewCard.layer.cornerRadius = 10
+        viewCard.layer.shadowColor = UIColor.black.cgColor
+        viewCard.layer.shadowOffset = CGSize(width: 2, height: 2)
+        viewCard.layer.shadowOpacity = 0.7
+        viewCard.layer.shadowRadius = 4.0
+        viewInfo.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
