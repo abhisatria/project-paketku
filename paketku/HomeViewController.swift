0d5a1e50a2ddfda8898e130ea68be1d03ed126c3
Gratianus Martin Brianto <50682978+martinbrianto@users.noreply.github.com>
2020-12-17 22:00:04 +0700

    Fungsionalitas Home

--- paketku/HomeViewController.swift
+++ paketku/HomeViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    enum couriers : String {
+        case JnT = "JnT"
+        case JNE = "JNE"
+        case Tiki = "Tiki"
+        case SiCepat = "SiCepat"
+        case NinjaExpress = "Ninja Express"
+        case Wahana = "Wahana"
+        case PosIndonesia = "POS Indonesia"
+    }
+    
+    var jsonData: CekResi?
+    var selectedCourier: String?
 
     @IBOutlet var btnCourier: [UIButton]!
+    
+    @IBOutlet weak var tfAWB: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,17 +43,7 @@ class HomeViewController: UIViewController {
         }
     }
     
-    enum couriers : String {
-        case JnT = "JnT"
-        case JNE = "JNE"
-        case Tiki = "Tiki"
-        case SiCepat = "SiCepat"
-        case NinjaExpress = "Ninja Express"
-        case Wahana = "Wahana"
-        case PosIndonesia = "POS Indonesia"
-        
-        
-    }
+    
     @IBAction func tappedCourier(_ sender: UIButton) {
         guard  let title = sender.currentTitle, let courier = couriers(rawValue: title) else {
             return
@@ -47,20 +52,102 @@ class HomeViewController: UIViewController {
         switch courier {
         case .JnT:
             print("JnT")
+            selectedCourier = "jnt"
         case .JNE:
             print("JNE")
+            selectedCourier = "jne"
         case .Tiki:
             print("Tiki")
+            selectedCourier = "tiki"
         case .SiCepat:
             print("Si Cepat")
+            selectedCourier = "sicepat"
         case .NinjaExpress:
             print("Ninja Express")
+            selectedCourier = "ninja"
         case .Wahana:
             print("Wahana")
+            selectedCourier = "wahana"
         case .PosIndonesia:
             print("POS Indonesia")
+            selectedCourier = "pos"
         }
     }
+    
+    
+    @IBAction func btnTrack(_ sender: Any) {
+        let awb = tfAWB.text as String?
+        if awb == ""{
+            showAlert(title: "Perhatian", message: "Anda harus mengisi nomor resi")
+            return
+        }
+        guard (selectedCourier != nil) else{
+            showAlert(title: "Perhatian", message: "Anda harus memilih kurir")
+            return
+        }
+        
+        loadData(awb!, selectedCourier!)
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "trackSegue"){
+            let destination = segue.destination as! DetailPaketViewController
+            destination.jsonData = self.jsonData
+        }
+    }
+    
+    func loadData(_ awb: String, _ courier: String) {
+            
+            var request = URLRequest(url: ApiRequest.init(awb: awb, courier: courier).resourceURL)
+            
+            request.httpMethod = "GET"
+            
+            let task =  URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                
+    
+                do{
+                    self.jsonData = try JSONDecoder().decode(CekResi.self, from: data!)
+                }
+                catch{
+                    print("json failed \(error.localizedDescription)")
+                }
+    
+                guard let json = self.jsonData else {
+                    return
+                }
+    
+                print(json.status)
+                print(json.message)
+                
+                DispatchQueue.main.async {
+                    
+                    if json.status == 400{
+                        self.showAlert(title: "Terjadi Kesalahan", message: "\(self.jsonData?.message) Periksa kembali nomor resi dan kurir anda")
+                    }
+                    
+                    self.performSegue(withIdentifier: "trackSegue", sender: self)
+                    
+                }
+    
+                
+            })
+            task.resume()
+        
+        }
+    
+    func showAlert(title: String,message: String){
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+            let yaAction = UIAlertAction(title: "Mengerti", style: .default, handler: nil)
+
+
+            alert.addAction(yaAction)
+            present(alert,animated: true,completion: nil)
+        }
+    
+    
     /*
     // MARK: - Navigation
 
